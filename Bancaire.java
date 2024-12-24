db0d787f5e793efa8e40b0b0790a1ee56dde3ba1
ridaDev <kiratterida@gmail.com>
2024-12-24 16:00:54 +0100

    créer MenuPrincipal

--- Bancaire.java
+++ Bancaire.java
@@ -1,6 +1,16 @@
 public class Bancaire {
 
+    static void MenuPrincipal(){
+        System.out.println("===== Menu Principal =====");
+        System.out.println("1.Gérer Clients");
+        System.out.println("2.Gérer Comptes");
+        System.out.println("3.Gérer Operations");
+        System.out.println("4.Filtrage");
+        System.out.println("5.Quitter");
+
+    }
+
     public static void main(String[] args) {
-        System.out.println("Bonjour");
+        MenuPrincipal();
     }
 }
