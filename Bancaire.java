21a9f3fcf098d74494a193dab2f191fbdf644a98
ridaDev <kiratterida@gmail.com>
2024-12-24 16:56:24 +0100

    créer GérerClient

--- Bancaire.java
+++ Bancaire.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Bancaire {
 
     static void MenuPrincipal(){
@@ -11,6 +13,47 @@ public class Bancaire {
     }
 
     public static void main(String[] args) {
-        MenuPrincipal();
+        int choix;
+        Scanner scannere = new Scanner(System.in);
+
+
+
+        do {
+            MenuPrincipal();
+            System.out.print("Bienvenue dans le Bancaire Entrer votre choix :");
+             choix = scannere.nextInt();
+            switch (choix){
+                case 1://Gérer Clients
+                    GereClient(choix);
+                    break;
+                case 2://Gérer Comptes
+                    break;
+                case 3://Gérer Operations
+                    break;
+                case 4://Filtrage
+                    break;
+                case 5:// Quitter
+                    break;
+                default:
+                    System.out.println("Choix incorrect");
+            }
+
+        }while (choix != 5);
+
     }
+
+    static void GereClient(int choixOption){
+        System.out.println("Gere Client");
+        switch (choixOption){
+            case 1:// Ajouter Client
+                break;
+            case 2:// Affichier Client
+                break;
+        }
+
+
+
+    }
+
+
 }
